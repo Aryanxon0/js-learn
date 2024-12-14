c7689a48fc7f858c6412024ecf5c17ed3dd60bc1
Aryanxon0 <aryanpaghda@gmail.com>
2024-12-14 09:53:05 +0530

    js assignment

---
+++ 01_variabels.js
@@ -0,0 +1,20 @@
+const accountId = 25255
+let accountEmail = "paghdaaryanxonn@gmail.com"
+let accountpassword = "2121"
+accountCity = "Surat"
+let accountstate ;
+
+// accountId = 3 
+
+accountEmail = "ap@gmail.com"
+accountpassword = "1234"
+accountCity = "jamnagar"
+
+/* not use var  */
+
+
+
+
+// console.log(accountId); 
+
+console.table([accountId ,accountEmail ,accountpassword ,accountCity ,accountstate])
